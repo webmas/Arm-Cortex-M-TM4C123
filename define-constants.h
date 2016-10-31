e901686db49eff39ebbaf88c21c35ea8267c89f3
Stefano Tondi <mieicorsi@libero.it>
2016-10-31 10:55:45 +0100

    Create define-constants.h

---
+++ define-constants.h
@@ -0,0 +1,21 @@
+// Definition of memory addresses for SysTick timer registers
+// |:aaa:|
+// | Address  | :31-24: | :23-17:|:16:|
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |
+
+
+// Address    31-24   23-17   16    15-3    2        1      0         Name 
+// $E000E010     0      0    Count    0   CLK_SRC  INTEN  ENABLE  NVIC_ST_CTRL_R
+
+
+| Command | Description |
+| --- | --- |
+| git status | List all new or modified files |
+| git diff | Show file differences that haven't been staged |
+
+
+#define NVIC_ST_CTRL_R      (*((volatile unsigned long *)0xE000E010)) // SysTick Control and Status Register
+#define NVIC_ST_RELOAD_R    (*((volatile unsigned long *)0xE000E014)) // RELOAD Register
+#define NVIC_ST_CURRENT_R   (*((volatile unsigned long *)0xE000E018)) // CURRENT Register
